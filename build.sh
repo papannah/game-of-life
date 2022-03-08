6801300e5597473d901ea405a372ad942e5fa11d
Hemachandra <hemachandrareddy442@gmail.com>
2022-03-08 11:44:20 +0530

    Create Build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#ssh ubuntu@172.31.47.44
+sudo systemctl status tomcat.service
+who
+pwd
+scp -r -P 22 /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ubuntu@172.31.47.44:/home/ubuntu/
+ssh ubuntu@172.31.47.44
+sudo systemctl status tomcat.service
+ls
+cd /opt/tomcat/
+ls
+cp /home/ubuntu/gameoflife.war ./webapps/
