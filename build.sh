4c14f2d47fd35a62601a6bedb30aefa074648534
ashokkumarpapireddy <93004448+ashokkumarpapireddy@users.noreply.github.com>
2022-03-09 11:36:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ whoami
 pwd
 mvn clean install
 echo "SCP Started"
-scp "/var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war" ubuntu@172.31.47.44:/home/ubuntu/
+'scp /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ubuntu@172.31.47.44:/home/ubuntu/'
 echo "SCP Done"
 ssh ubuntu@172.31.47.44
 'hostname;
