df32cfab2e35177ff76825ad541b0e9f0d00a594
Hemachandra <hemachandrareddy442@gmail.com>
2022-03-09 11:17:32 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ whoami
 pwd
 mvn clean install
 echo "SCP Started"
-sudo su 
 scp "/var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war" ubuntu@172.31.47.44:/home/ubuntu/
 echo "SCP Done"
 ssh ubuntu@172.31.47.44
