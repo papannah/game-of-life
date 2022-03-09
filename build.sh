1238cf9344c415f424f0ecf1527b0ea03ed94022
Hemachandra <hemachandrareddy442@gmail.com>
2022-03-09 10:52:10 +0530

    Updated

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ hostname
 whoami
 pwd
 mvn clean install
+echo "SCP Started"
 scp /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ubuntu@172.31.47.44:/home/ubuntu/
+echo "SCP Done"
 ssh ubuntu@172.31.47.44
 'hostname;
  sudo systemctl status tomcat.service;
