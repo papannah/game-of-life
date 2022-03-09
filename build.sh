c97f9c6259d887677827047f8e503e383a6deb67
Hemachandra <hemachandrareddy442@gmail.com>
2022-03-09 09:34:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ pwd
 scp -r -P 22 /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ubuntu@172.31.47.44:/home/ubuntu/
 ssh ubuntu@172.31.47.44
 'hostname;
-sudo systemctl status tomcat.service;
-cd /opt/tomcat/;
-ls;
-cp /home/ubuntu/gameoflife.war ./webapps/'
+ sudo systemctl status tomcat.service;
+ cd /opt/tomcat/;
+ ls;
+ cp /home/ubuntu/gameoflife.war ./webapps/'
