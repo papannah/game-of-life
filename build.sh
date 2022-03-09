ceb26946c1d3c7a2f2196cd60961d5fb7e800d1b
Hemachandra <hemachandrareddy442@gmail.com>
2022-03-09 11:51:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ echo "SCP Started"
 #sudo chmod +x /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war
 scp /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ubuntu@172.31.47.44:/home/ubuntu
 echo "SCP Done"
-ssh ubuntu@172.31.47.44 "
+ssh ubuntu@172.31.47.44 '
 hostname;
 sudo systemctl status tomcat.service;
 cd /opt/tomcat/;
 ls;
 cp /home/ubuntu/gameoflife.war /opt/tomcat/webapps/
-"
+'
